d61b1da51962fa5ab10e42b3ccf313db466c4338
Jefferson Rodrigues <jeffersonhfr@gmail.com>
2021-09-06 11:40:55 -0300

    feature: criando router Post

--- server/routes/products.js
+++ server/routes/products.js
@@ -4,7 +4,8 @@ var productsController = require('../controllers/products');
 
 /* GET users listing. */
 router.get('/', productsController.index);
-router.get('/adicionar', productsController.add);
+router.get('/adicionar', productsController.addProduct);
+router.post('/adicionar', productsController.guardarProduto);
 router.get('/:id?', productsController.show);
 router.get('/:id/edit', productsController.edit);
 router.get('/:id/delete', productsController.delete);
